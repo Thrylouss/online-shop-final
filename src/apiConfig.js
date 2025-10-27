9fd1d7bd10d93b03e45a140c3b1fd2b54de6e4c0
unknown <dilshoditolmasov32@gmail.com>
2025-10-27 10:54:52 +0500

    project is updated

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,3 @@
-// apiConfig.js
-const API_BASE_URL = 'https://online-shop.milliybiz.uz'; // основной домен
+const API_BASE_URL = 'https://online-shop.milliybiz.uz'; 
 
 export default API_BASE_URL;
--- src/context/ProtectedRoute.jsx
+++ src/context/ProtectedRoute.jsx
@@ -5,8 +5,6 @@ import '../assets/scss/loading.scss'
 
 const ProtectedRoute = ({ children }) => {
     const { userMe, error, loading } = useAuthMe();
-
-    // Пока грузим информацию о пользователе, можем отобразить спиннер или скелетон:
     if (loading) {
         return <div className="loading">
             <div className="i"></div>
@@ -15,13 +13,10 @@ const ProtectedRoute = ({ children }) => {
         </div>;
     }
 
-    // Если есть ошибка, или userMe так и не загрузился (=== null),
-    // то перенаправляем на страницу авторизации:
     if (error || !userMe) {
         return <Navigate to="/auth" replace />;
     }
 
-    // Если всё ок (userMe есть, нет ошибки), рендерим дочерние компоненты
     return children;
 };
 
